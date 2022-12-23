0a91976fb5d1bdab4e6e87f1f254e9eca93a084c
whathebea <bea.graciast@gmail.com>
2022-12-23 17:17:09 -0300

    first commit: added spring boot starter project

---
+++ src/main/java/com/whathebea/timetrack/timetrack/TimeTrackApplication.java
@@ -0,0 +1,13 @@
+package com.whathebea.timetrack.timetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whathebea/timetrack/timetrack/TimeTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whathebea.timetrack.timetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
